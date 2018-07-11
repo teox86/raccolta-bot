87f0a2782fcfe085300cd215cfd9deb15aca66c5
teox86 <teox86@gmail.com>
2018-07-11 22:22:47 +0200

    New commands. Added calendar

--- RaccoltaBot/Program.cs
+++ RaccoltaBot/Program.cs
@@ -22,6 +22,7 @@ namespace RaccoltaBot
 
         static Timer timer;
 
+        private static List<GiornoRaccolta> Calendario = null;
 
         public static void Main(string[] args)
         {
@@ -60,76 +61,76 @@ namespace RaccoltaBot
 
             switch (message.Text.Split(' ').First())
             {
-                // send inline keyboard
-                case "/inline":
-                    await Bot.SendChatActionAsync(message.Chat.Id, ChatAction.Typing);
+                //// send inline keyboard
+                //case "/inline":
+                //    await Bot.SendChatActionAsync(message.Chat.Id, ChatAction.Typing);
 
-                    await Task.Delay(500); // simulate longer running task
+                //    await Task.Delay(500); // simulate longer running task
 
-                    var inlineKeyboard = new InlineKeyboardMarkup(new[]
-                    {
-                        new [] // first row
-                        {
-                            InlineKeyboardButton.WithCallbackData("1.1"),
-                            InlineKeyboardButton.WithCallbackData("1.2"),
-                        },
-                        new [] // second row
-                        {
-                            InlineKeyboardButton.WithCallbackData("2.1"),
-                            InlineKeyboardButton.WithCallbackData("2.2"),
-                        }
-                    });
+                //    var inlineKeyboard = new InlineKeyboardMarkup(new[]
+                //    {
+                //        new [] // first row
+                //        {
+                //            InlineKeyboardButton.WithCallbackData("1.1"),
+                //            InlineKeyboardButton.WithCallbackData("1.2"),
+                //        },
+                //        new [] // second row
+                //        {
+                //            InlineKeyboardButton.WithCallbackData("2.1"),
+                //            InlineKeyboardButton.WithCallbackData("2.2"),
+                //        }
+                //    });
 
-                    await Bot.SendTextMessageAsync(
-                        message.Chat.Id,
-                        "Choose",
-                        replyMarkup: inlineKeyboard);
-                    break;
+                //    await Bot.SendTextMessageAsync(
+                //        message.Chat.Id,
+                //        "Choose",
+                //        replyMarkup: inlineKeyboard);
+                //    break;
 
-                // send custom keyboard
-                case "/keyboard":
-                    ReplyKeyboardMarkup ReplyKeyboard = new[]
-                    {
-                        new[] { "1.1", "1.2" },
-                        new[] { "2.1", "2.2" },
-                    };
+                //// send custom keyboard
+                //case "/keyboard":
+                //    ReplyKeyboardMarkup ReplyKeyboard = new[]
+                //    {
+                //        new[] { "1.1", "1.2" },
+                //        new[] { "2.1", "2.2" },
+                //    };
 
-                    await Bot.SendTextMessageAsync(
-                        message.Chat.Id,
-                        "Choose",
-                        replyMarkup: ReplyKeyboard);
-                    break;
+                //    await Bot.SendTextMessageAsync(
+                //        message.Chat.Id,
+                //        "Choose",
+                //        replyMarkup: ReplyKeyboard);
+                //    break;
 
-                // send a photo
-                case "/photo":
-                    await Bot.SendChatActionAsync(message.Chat.Id, ChatAction.UploadPhoto);
+                //// send a photo
+                //case "/photo":
+                //    await Bot.SendChatActionAsync(message.Chat.Id, ChatAction.UploadPhoto);
 
-                    const string file = @"Files/tux.png";
+                //    const string file = @"Files/tux.png";
 
-                    var fileName = file.Split(Path.DirectorySeparatorChar).Last();
+                //    var fileName = file.Split(Path.DirectorySeparatorChar).Last();
 
-                    using (var fileStream = new FileStream(file, FileMode.Open, FileAccess.Read, FileShare.Read))
-                    {
-                        await Bot.SendPhotoAsync(
-                            message.Chat.Id,
-                            fileStream,
-                            "Nice Picture");
-                    }
-                    break;
+                //    using (var fileStream = new FileStream(file, FileMode.Open, FileAccess.Read, FileShare.Read))
+                //    {
+                //        await Bot.SendPhotoAsync(
+                //            message.Chat.Id,
+                //            fileStream,
+                //            "Nice Picture");
+                //    }
+                //    break;
 
-                // request location or contact
-                case "/request":
-                    var RequestReplyKeyboard = new ReplyKeyboardMarkup(new[]
-                    {
-                        KeyboardButton.WithRequestLocation("Location"),
-                        KeyboardButton.WithRequestContact("Contact"),
-                    });
+                //// request location or contact
+                //case "/request":
+                //    var RequestReplyKeyboard = new ReplyKeyboardMarkup(new[]
+                //    {
+                //        KeyboardButton.WithRequestLocation("Location"),
+                //        KeyboardButton.WithRequestContact("Contact"),
+                //    });
 
-                    await Bot.SendTextMessageAsync(
-                        message.Chat.Id,
-                        "Who or Where are you?",
-                        replyMarkup: RequestReplyKeyboard);
-                    break;
+                //    await Bot.SendTextMessageAsync(
+                //        message.Chat.Id,
+                //        "Who or Where are you?",
+                //        replyMarkup: RequestReplyKeyboard);
+                //    break;
 
                 case "/register":
 
@@ -165,11 +166,63 @@ namespace RaccoltaBot
                         replyMarkup: new ReplyKeyboardRemove());
                     break;
 
+                case "/domani":
+
+                    await Bot.SendTextMessageAsync(
+                        message.Chat.Id,
+                        MsgRaccoltaTraXGiorni(1),
+                        replyMarkup: new ReplyKeyboardRemove());
+
+                    break;
+
+                case "/tra":
+
+                    var args = message.Text.Split(' ');
+                    int days = 0;
+
+                    string res = "Uso: /tra X";
+
+                    if (args.Length == 2 && Int32.TryParse(args[1], out days))
+                        res = MsgRaccoltaTraXGiorni(days);
+
+                    await Bot.SendTextMessageAsync(
+                        message.Chat.Id,
+                        res,
+                        replyMarkup: new ReplyKeyboardRemove());
+
+                    break;
+
+                case "/prossime":
+
+                    var culture = new System.Globalization.CultureInfo("it-IT");
+
+                    string nxt = "";
+
+                    for (int i = 1; i < 8; i++)
+                    {
+                        GiornoRaccolta gr = GetRaccolta(i);
+                        if (gr.Raccolta.Count > 0)
+                            nxt += string.Format("{0} raccolta {1}",
+                                culture.DateTimeFormat.GetDayName(DateTime.Today.AddDays(i).DayOfWeek), string.Join(",", gr.Raccolta.ToArray())) + Environment.NewLine;
+                        else
+                            nxt += string.Format("{0}",
+                                culture.DateTimeFormat.GetDayName(DateTime.Today.AddDays(i).DayOfWeek)) + Environment.NewLine;
+                    }
+
+                    await Bot.SendTextMessageAsync(
+                        message.Chat.Id,
+                        nxt,
+                        replyMarkup: new ReplyKeyboardRemove());
+
+                    break;
+
                 default:
                     const string usage = @"
-                        Usage:
-                        /register   - Ricevi notifiche
-                        /unregister - Silenzia notifiche";
+Usage:
+/register   - Ricevi notifiche
+/unregister - Silenzia notifiche
+/domani - Raccolta di domani
+/prossime - Prossime raccolte";
 
                     await Bot.SendTextMessageAsync(
                         message.Chat.Id,
@@ -245,7 +298,7 @@ namespace RaccoltaBot
             Console.WriteLine("### Timer Started ###");
 
             DateTime nowTime = DateTime.Now;
-            DateTime scheduledTime = new DateTime(nowTime.Year, nowTime.Month, nowTime.Day, nowTime.AddMinutes(5).Hour, nowTime.AddMinutes(5).Minute, 0, 0); //Specify your scheduled time HH,MM,SS
+            DateTime scheduledTime = new DateTime(nowTime.Year, nowTime.Month, nowTime.Day, 19, 30, 0, 0); //Specify your scheduled time HH,MM,SS
             if (nowTime > scheduledTime)
             {
                 scheduledTime = scheduledTime.AddDays(1);
@@ -261,23 +314,122 @@ namespace RaccoltaBot
         {
             Console.WriteLine("### Timer Stopped ### \n");
             timer.Stop();
+            string resp = MsgRaccoltaTraXGiorni(1);
 
             List<RegisteredUsers> listUsers = JsonConvert.DeserializeObject<List<RegisteredUsers>>(File.ReadAllText(UsersFile));
             foreach (RegisteredUsers u in listUsers)
             {
                 await Bot.SendTextMessageAsync(
                     u.ChatID,
-                    "timout",
+                    resp,
                     replyMarkup: new ReplyKeyboardRemove());
             }
 
             schedule_Timer();
         }
 
+        private static string MsgRaccoltaTraXGiorni(int days)
+        {
+            GiornoRaccolta tomorrow = GetRaccolta(days);
+
+            string resp = @"Nessuna info per domani";
+
+            if (tomorrow != null)
+            {
+                if (tomorrow.Raccolta.Count == 0)
+                    resp = @"Nessuna raccolta domani";
+                else
+                {
+                    if (days == 1)
+                        resp = @"Domani raccolta " + string.Join(",", tomorrow.Raccolta.ToArray());
+                    else
+                        resp = string.Format(@"Tra {0} giorni, raccolta {1}", days, string.Join(",", tomorrow.Raccolta.ToArray()));
+                }
+            }
+
+            return resp;
+        }
+
+        private static GiornoRaccolta GetRaccolta(int days)
+        {
+            if (Calendario == null)
+                Calendario = ReadFromCsv();
+
+            GiornoRaccolta raccolta = Calendario.Where(x => x.Giorno == DateTime.Today.AddDays(days)).FirstOrDefault();
+
+            return raccolta;
+        }
+
+        private static List<GiornoRaccolta> ReadFromCsv()
+        {
+            List<GiornoRaccolta> res = new List<GiornoRaccolta>();
+
+            using (var reader = new StreamReader(@"calendario_raccolta.csv"))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    GiornoRaccolta gr = new GiornoRaccolta();
+
+                    var values = line.Split(',');
+
+                    gr.Giorno = DateTime.Parse(values[0]);
+
+                    for (int i = 1; i < values.Length; i++)
+                    {
+                        gr.AggiungiRaccolta(values[i]);
+                    }
+
+                    res.Add(gr);
+                }
+            }
+
+            return res;
+        }
     }
 
     public class RegisteredUsers
     {
         public string ChatID { get; set; }
+    }
+
+    public class GiornoRaccolta
+    {
+        public DateTime Giorno { get; set; }
+        public List<TipoRaccolta> Raccolta { get; set; }
+
+        public void AggiungiRaccolta(string cod)
+        {
+            if (Raccolta == null)
+                Raccolta = new List<TipoRaccolta>();
+
+            switch (cod)
+            {
+                case "ca":
+                    Raccolta.Add(TipoRaccolta.Carta);
+                    break;
+                case "pl":
+                    Raccolta.Add(TipoRaccolta.Plastica);
+                    break;
+                case "or":
+                    Raccolta.Add(TipoRaccolta.Organico);
+                    break;
+                case "ve":
+                    Raccolta.Add(TipoRaccolta.Vetro);
+                    break;
+                case "in":
+                    Raccolta.Add(TipoRaccolta.Indifferenziato);
+                    break;
+            }
+        }
+    }
+
+    public enum TipoRaccolta
+    {
+        Carta,
+        Plastica,
+        Vetro,
+        Organico,
+        Indifferenziato
     }
 }
